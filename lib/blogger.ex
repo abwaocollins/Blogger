306c506450bccfc39eaca3361a80d010470d2528
abwaocollins <44702795+abwaocollins@users.noreply.github.com>
2022-09-08 20:07:51 +0300

    changes on implementation

--- lib/blogger.ex
+++ lib/blogger.ex
@@ -9,36 +9,25 @@ defmodule Blogger do
   # function that takes input list and returns the list in uppercase
   def to_upper(list \\ []) do
 
-    Enum.map(list, fn x -> String.upcase(x) end)
+    Enum.map(list, &(String.upcase(&1)))
 
   end
 
-  # question 3
-  def get_title() do
-    HTTPoison.get! "https://jsonplaceholder.typicode.com/todos/1"
-  end
 
   # Function to print to the console the todo name, pass the url as an argument when running from the portal
-  def get(url, headers \\ []) do
+  def get_title() do
 
-    url
-    |> HTTPoison.get(headers)
-    |> case do
+    case HTTPoison.get "https://jsonplaceholder.typicode.com/todos/1" do
         {:ok, %{body: raw, status_code: code}} -> {code, raw}
         {:error, %{reason: reason}} -> {:error, reason}
       end
-    |> (fn {ok, body} ->
-          body
-          |> Poison.decode(keys: :atoms)
-          |> case do
+      |> case do
+        {_ok, body} -> Poison.decode(body)
+      end
+      |> fn
+        {_ok, map} -> IO.puts map["title"]
+      end.()
 
-              {:ok, parsed} -> {ok, parsed}
-
-              IO.puts(parsed.title)
-
-              _ -> {:error, body}
-            end
-        end).()
 
 
     end
